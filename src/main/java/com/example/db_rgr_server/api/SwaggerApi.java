3b60f638361f53cd676ece3b965f7741ad76b1b0
etroshkova <Ekaterina.Troshkova@noveogroup.com>
2020-12-08 11:19:32 +0700

    Magic to create request with body in swagger

--- src/main/java/com/example/db_rgr_server/api/SwaggerApi.java
+++ src/main/java/com/example/db_rgr_server/api/SwaggerApi.java
@@ -2,16 +2,13 @@ package com.example.db_rgr_server.api;
 
 import com.example.db_rgr_server.model.Good;
 import com.example.db_rgr_server.repository.GoodRepository;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.List;
 
-@Api(value = "/get_goods")
+@Api(value = "Good")
 public interface SwaggerApi {
     @Path("/get_goods")
     @GET
@@ -23,4 +20,15 @@ public interface SwaggerApi {
             @ApiResponse(code = 204, message = "NO CONTENT -> The user has been removed successfully")
     })
     public List<Good> loadAll();
+
+    @Path("/add_good")
+    @GET
+    @ApiOperation(httpMethod = "POST", value = "Resource to get a list of goods")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK -> The user has been fetched successfully"),
+            @ApiResponse(code = 201, message = "OK -> The user has been created successfully"),
+            @ApiResponse(code = 202, message = "Accepted -> The user has been updated successfully"),
+            @ApiResponse(code = 204, message = "NO CONTENT -> The user has been removed successfully")
+    })
+    public void save(@ApiParam("good") Good good);
 }
