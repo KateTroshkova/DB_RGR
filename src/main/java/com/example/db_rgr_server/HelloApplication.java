3c8b1a6343f204e85ed35073e493660288909b1a
etroshkova <Ekaterina.Troshkova@noveogroup.com>
2020-12-08 09:58:59 +0700

    Magic to generate swagger.json

--- src/main/java/com/example/db_rgr_server/HelloApplication.java
+++ src/main/java/com/example/db_rgr_server/HelloApplication.java
@@ -1,9 +1,30 @@
 package com.example.db_rgr_server;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/")
 public class HelloApplication extends Application {
 
+    public HelloApplication(){
+        System.out.println("start swagger generation");
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("com.example.db_rgr_server");
+        beanConfig.setScan(true);
+        System.out.println("complete swagger generation");
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
+    }
 }
